93de7dbabb1397c6a2cb2b23397c2149164af2fd
Nicolas Peitton <nicolaspeitton@gmail.com>
2024-02-05 19:52:56 -0300

    add images tmbd

--- js/tmdb.js
+++ js/tmdb.js
@@ -6,38 +6,63 @@ const options = {
     }
   };
   
-  fetch('https://api.themoviedb.org/3/authentication', options)
+  
+  function aut (){
+    fetch('https://api.themoviedb.org/3/authentication', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+  }
 
+  function insertar(url){
+    const section = document.querySelector("#lista");
+    section.insertAdjacentHTML(
+      "beforeend",
+      `<img style="width:150px" src=${url} alt=${url}>`
+    )
+  }  
+
+function movies() {
+  
+  
   fetch('https://api.themoviedb.org/3/discover/movie?language=en-US&year=2023', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data.results)
+    // let result = data.results[0].poster_path;
+    data.results.map(movie => {
+      insertar("https://image.tmdb.org/t/p/w500"+movie.poster_path)
+    }) 
+  })
+  .catch(err => console.error(err));
+  
+}
+ 
+movies();
 
-    const cuerpo = {
-      id: 572802
-    }
 
 
-        
-      const optionsPost = {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NThkMGQ3NzFkYzMzY2E1ODliYTVlMDBhNTMyMmM4YyIsInN1YiI6IjYyYjllZDU5MjI5YWUyMTIxM2FjY2UyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GsK0NJ0Au0j6fnZonB-baDe_Bs1EcT9GePGjC-dnb2A'
-        },
-        body: JSON.stringify(cuerpo)
-        
-      };
+
+
+
+ function favorite (){ 
+   const optionsPost = {
+     method: 'POST',
+     headers: {
+       accept: 'application/json',
+       'content-type': 'application/json',
+       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NThkMGQ3NzFkYzMzY2E1ODliYTVlMDBhNTMyMmM4YyIsInN1YiI6IjYyYjllZDU5MjI5YWUyMTIxM2FjY2UyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GsK0NJ0Au0j6fnZonB-baDe_Bs1EcT9GePGjC-dnb2A'
+      },
+      body: JSON.stringify(cuerpo)
       
-      fetch('https://api.themoviedb.org/3/account/12921085/favorite', optionsPost)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-
-
-
+    };
+    
+    fetch('https://api.themoviedb.org/3/account/12921085/favorite', optionsPost)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  } 
+    
+    
+    
         
